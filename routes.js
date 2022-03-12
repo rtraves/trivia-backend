3d5bf1dd0d198bd93c3eded70dc6b8607089d138
Richard <prixec@gmail.com>
2022-03-12 18:52:21 -0500

    finished updating routes

--- routes.js
+++ routes.js
@@ -15,9 +15,20 @@ router.get('/questions', async (req, res) => {
 });
 
 // get one quiz question
-router.get('/questions/:id', (req, res) => {
+router.get('/questions/:id', async (req, res) => {
+    try {
+        const _id = req.params.id
 
-});
+        const question = await Question.findOne({_id})        
+        if(!question){
+            return res.status(404).json({})
+        }else{
+            return res.status(200).json(question)
+        }
+    } catch (error) {
+        return res.status(500).json({"error":error})
+    }
+})
 
 // create one quiz question
 router.post('/questions', async (req, res) => {
@@ -42,13 +53,49 @@ router.post('/questions', async (req, res) => {
 })
 
 // update one quiz question
-router.put('/questions/:id', (req, res) => {
+router.put('/questions/:id', async (req, res) => {
+    try {
+        const _id = req.params.id 
+        const { text, answer, category, isValid } = req.body
 
-});
+        let question = await Question.findOne({_id})
+
+        if(!question){
+            question = await Question.create({
+                text,
+                answer,
+                category,
+                isValid
+            })
+            return res.status(201).json(question)
+        }else{
+            question.text = text,
+            question.answer = answer,
+            question.category = category,
+            question.isValid = isValid,
+            await question.save()
+            return res.status(200).json(question)
+        }
+    } catch (error) {
+        return res.status(500).json({"error":error})
+    }
+})
 
 // delete one quiz question
-router.delete('/questions/:id', (req, res) => {
+router.delete('/questions/:id', async (req, res) => {
+    try {
+        const _id = req.params.id 
 
-});
+        const question = await Question.deleteOne({_id})
+
+        if(question.deletedCount === 0){
+            return res.status(404).json()
+        }else{
+            return res.status(204).json()
+        }
+    } catch (error) {
+        return res.status(500).json({"error":error})
+    }
+})
 
 module.exports = router;
