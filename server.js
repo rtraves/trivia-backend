a2411c242b096aed20c2493d6fb7d4c143da978c
Richard <prixec@gmail.com>
2022-04-18 21:49:20 -0400

    added jest and modified get route for valid q's

--- server.js
+++ server.js
@@ -6,23 +6,13 @@ const cors = require('cors');
 
 require('dotenv').config();
 
-// Add headers before the routes are defined
 app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
+  // CORS stuff
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
   next();
 });
 
@@ -31,7 +21,7 @@ app.use(routes);
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
@@ -39,4 +29,4 @@ db.once('open', () => console.log('database connected'));
 
 app.listen(process.env.PORT, () => {
   console.log("The API is running on port " + process.env.PORT+"...");
-})
+});
